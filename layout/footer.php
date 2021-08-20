ba4f2618b71db4bf29561b72d17000bd9b382a6c
Mohammad Abdul Bashir <67291611+Basir37@users.noreply.github.com>
2021-08-21 04:37:13 +0600

    add

---
+++ layout/footer.php
@@ -0,0 +1,6 @@
+<footer class="py-5 bg-dark">
+  <div class="container">
+    <p class="m-0 text-center text-white">Copyright &copy; <a href=""> Basir 2021</a></p>
+  </div>
+  <!-- /.container -->
+</footer>
